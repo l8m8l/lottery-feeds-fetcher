76a0c64f19c289af2fd8bca049f93d7ddb5a2ccc
jason.chung <shiningjason1989@gmail.com>
2021-02-16 16:12:07 +0800

    feat: add web scraping endpoint

--- src/index.js
+++ src/index.js
@@ -3,17 +3,40 @@ require('@0y0/scraper-extensions')
 const getPhenixFeed = require('./feeds/phenix')
 const { LotType } = require('./enum')
 require('./env')
-const healthCheck = require('./healthCheck')
+const runServer = require('./server')
 const logger = require('./logger')
 
-healthCheck()
+function getFeed(type) {
+  switch (type) {
+    case LotType.PK10LA:
+    case LotType.PK10BJ:
+    case LotType.LHHK:
+    case LotType.K3L3:
+    case LotType.D3W:
+      return getPhenixFeed(type)
+  }
+  throw new Error(`Unsupported lottery type '${type}'`)
+}
+
+function scrapeAsync(type) {
+  return new Promise((resolve, reject) => {
+    try {
+      runScraper(getFeed(type), error => (error ? reject(error) : resolve()))
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+runServer({ scrapeAsync })
+
 runScraper(
   [
-    getPhenixFeed(LotType.PK10LA),
-    getPhenixFeed(LotType.PK10BJ),
-    getPhenixFeed(LotType.LHHK),
-    getPhenixFeed(LotType.K3L3),
-    getPhenixFeed(LotType.D3W)
+    getFeed(LotType.PK10LA),
+    getFeed(LotType.PK10BJ),
+    getFeed(LotType.LHHK),
+    getFeed(LotType.K3L3),
+    getFeed(LotType.D3W)
   ],
   (error, opt) => {
     if (error) logger.error(`[${opt.lotType}] ${error.message}`)
